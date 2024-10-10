c1a0823b061b89fd2bfc6bb25b3fc6cd4df62cef
Michael0303 <mmm323790@gmail.com>
2024-10-10 14:21:40 +0800

    change the project structure to 5GTQ

--- CQFscheduler/src/NRAmcpub.h
+++
@@ -1,8 +0,0 @@
-#include "stack/mac/amc/LteAmc.h"
-#include "stack/mac/amc/NRAmc.h"
-#include "stack/mac/amc/NRMcs.h"
-
-class NRAmcpub : public NRAmc {
-   public:
-    static NRMCSelem getMcsElemPerCqi(Cqi cqi, const Direction dir);
-};
